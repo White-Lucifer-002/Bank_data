cb18da15e438c8acc0e50ee6125d9d74b015f85f
White-Lucifer-002 <sriramrockzz2002@gmail.com>
2022-05-29 12:11:15 +0530

    Added bank data

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const client = require('./connectivity.js')
 const express = require('express');
 const app = express();
 
-var port_number = process.env.PORT || 3000;
+var port_number = process.env.PORT || 1234;
 
 app.listen(port_number,()=>{
     console.log(port_number)
@@ -20,26 +20,24 @@ app.listen(port_number,()=>{
 
 client.connect();
 
-app.get('/api/branch', (req, res)=>{
-    let query = res.body;
-    client.query(`SELECT * FROM bank_branches WHERE branch LIKE '${req.query.q}%' ORDER BY ifsc LIMIT ${req.query.limit} OFFSET ${req.query.offset};`, (err, result)=>{
+app.get('/api/branch', (request, response)=>{
+    client.query(`SELECT * FROM bank_branches WHERE branch LIKE '${request.query.q}%' ORDER BY ifsc LIMIT ${request.query.limit} OFFSET ${request.query.offset};`, (err, result)=>{
         if(!err){
-            res.send({"branches":result.rows});
+            response.send({"branches":result.rows});
         }
     });
     client.end;
 })
 
-app.get('/', (req, res)=>{
-            res.send("Hello");
+app.get('/', (request, response)=>{
+            response.send("Hello");
 })
 
 
-app.get('/api/search', (req, res)=>{
-    let query = res.body;
-    client.query(`SELECT * FROM bank_branches WHERE branch LIKE '%${req.query.q}%' OR city ILIKE '%${req.query.q}%' OR ifsc ILIKE '%${req.query.q}%' or district ILike '%${req.query.q}%' or state ILike '%${req.query.q}%'  order by ifsc LIMIT ${req.query.limit} OFFSET ${req.query.offset};`, (err, result)=>{
+app.get('/api/search', (request, response)=>{
+    client.query(`SELECT * FROM bank_branches WHERE branch LIKE '%${request.query.q}%' OR city ILIKE '%${request.query.q}%' OR ifsc ILIKE '%${request.query.q}%' or district ILike '%${request.query.q}%' or state ILike '%${request.query.q}%'  order by ifsc LIMIT ${request.query.limit} OFFSET ${request.query.offset};`, (err, result)=>{
         if(!err){
-            res.send({"branches":result.rows});
+            response.send({"branches":result.rows});
         }
     });
     client.end;
